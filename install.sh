1d76208ac3f17d8964b7bd6d0f9b106a15841162
itssali <woowooch123@gmail.com>
2024-10-08 14:31:13 +0300

    lol

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ echo " | |_| |/ _ \ | |/ _ \  | |\/| | '__| |  \| |/ _` / __/ __|/ _ \ '__|"
 echo " |  _  |  __/ | | (_) | | |  | | |_   | |\  | (_| \__ \__ \  __/ |   "
 echo " |_| |_|\___|_|_|\___/  |_|  |_|_(_)  |_| \_|\__,_|___/___/\___|_|   "
 echo ""
+
 echo "Installing AN Command Line..."
 
 # Move the executable to /usr/local/bin for global access
@@ -15,12 +16,3 @@ sudo mv an-cli /usr/local/bin/an
 echo ""
 echo "AN Command Line has been installed successfully!"
 echo "You can now use 'an' commands globally."
-
-
-
-  _   _      _ _         __  __        _   _                         
- | | | | ___| | | ___   |  \/  |_ __  | \ | | __ _ ___ ___  ___ _ __ 
- | |_| |/ _ \ | |/ _ \  | |\/| | '__| |  \| |/ _` / __/ __|/ _ \ '__|
- |  _  |  __/ | | (_) | | |  | | |_   | |\  | (_| \__ \__ \  __/ |   
- |_| |_|\___|_|_|\___/  |_|  |_|_(_)  |_| \_|\__,_|___/___/\___|_|   
-                                                                     
