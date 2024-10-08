cc6c7f979672dd7e062b498226ce2821ef2f2fd7
itssali <woowooch123@gmail.com>
2024-10-08 15:50:57 +0300

    update rn

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Download the executable using PowerShell
-powershell -Command "Invoke-WebRequest -Uri https://github.com/itssali/command-line -OutFile an.exe"  REM Replace with the actual URL of the executable
+powershell -Command "Invoke-WebRequest -Uri https://github.com/itssali/command-line/blob/main/install.bat -OutFile an.exe"  REM Replace with the actual URL of the executable
 
 REM Move the executable to Program Files
 move an.exe "C:\Program Files\an.exe"
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download the executable
-curl -LO https://github.com/itssali/command-line  # Replace with the actual URL of the executable
+curl -LO https://github.com/itssali/command-line/blob/main/an  # Replace with the actual URL of the executable
 
 # Move it to /usr/local/bin
 sudo mv an /usr/local/bin/an
